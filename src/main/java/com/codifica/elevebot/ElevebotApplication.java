5adb5ccc94f7d70cb8846fb166d7adaf8e3c3d2e
Felippe-ASLopes <felippedias3@gmail.com>
2025-02-18 11:43:45 -0300

    :heavy_plus_sign: build: Adicionando dependencias do Spring e MySQL

---
+++ src/main/java/com/codifica/elevebot/ElevebotApplication.java
@@ -0,0 +1,13 @@
+package com.codifica.elevebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElevebotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElevebotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codifica/elevebot/ElevebotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codifica.elevebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElevebotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
