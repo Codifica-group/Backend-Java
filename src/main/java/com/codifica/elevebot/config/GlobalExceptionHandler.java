4fa56ef1ebf94fef41266954ed28314932720aeb
Felippe-ASLopes <felippedias3@gmail.com>
2025-04-02 17:00:16 -0300

    :heavy_minus_sign: build: removendo problem-spring-web

--- src/main/java/com/codifica/elevebot/config/GlobalExceptionHandler.java
+++ src/main/java/com/codifica/elevebot/config/GlobalExceptionHandler.java
@@ -1,9 +1,8 @@
 package com.codifica.elevebot.config;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.zalando.problem.spring.web.advice.ProblemHandling;
 
 @ControllerAdvice
-public class GlobalExceptionHandler implements ProblemHandling {
+public class GlobalExceptionHandler {
 
 }
