65090cfbb5a723dbb6120a820c2d2ff007535cfc
Felippe-ASLopes <felippedias3@gmail.com>
2025-05-25 15:30:54 -0300

    :recycle: refact: Geração e validação do Token

--- src/main/java/com/codifica/elevebot/controller/TokenController.java
+++ src/main/java/com/codifica/elevebot/controller/TokenController.java
@@ -2,6 +2,7 @@ package com.codifica.elevebot.controller;
 
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.codifica.elevebot.service.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,12 +10,15 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/auth")
 public class TokenController {
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping("/validate")
     public ResponseEntity<String> validateToken(@RequestHeader("Authorization") String tokenHeader) {
         try {
             String token = tokenHeader.replace("Bearer ", "");
 
-            DecodedJWT jwt = TokenService.validateToken(token);
+            DecodedJWT jwt = tokenService.validateToken(token);
             String username = jwt.getSubject();
 
             return ResponseEntity.ok("Token válido para: " + username);
--- src/main/java/com/codifica/elevebot/service/TokenService.java
+++ src/main/java/com/codifica/elevebot/service/TokenService.java
@@ -2,26 +2,38 @@ package com.codifica.elevebot.service;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.auth0.jwt.interfaces.JWTVerifier;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+
 @Service
 public class TokenService {
-    private static final String SECRET_KEY = "Eleve";
+
+    private static final String SECRET_KEY = System.getenv("SECRET_KEY");
+    private static final String ISSUER = "elevebot";
+    private static final long EXPIRATION_TIME = 1000 * 60 * 60;
 
     public String generateToken(String username) {
         return JWT.create()
                 .withSubject(username)
-                .withIssuer("elevebot")
+                .withIssuer(ISSUER)
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME)) // Expira em 1 hora
                 .sign(Algorithm.HMAC256(SECRET_KEY));
     }
 
-    public static DecodedJWT validateToken(String token) {
-        Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
-        JWTVerifier verifier = JWT.require(algorithm)
-                .withIssuer("elevebot")
-                .build();
-        return verifier.verify(token);
+    public DecodedJWT validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .withIssuer(ISSUER)
+                    .build();
+            return verifier.verify(token);
+        } catch (JWTVerificationException e) {
+            throw new RuntimeException("Token inválido ou expirado", e);
+        }
     }
 }
